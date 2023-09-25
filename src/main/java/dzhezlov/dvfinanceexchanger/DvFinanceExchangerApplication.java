4f4a6735385ca15ec60bf044072aa466d444a334
Dmitrii Zhezlov <dzhezlov@een.com>
2023-09-24 23:29:54 -0500

    removed runner which clears trade history on startup

--- src/main/java/dzhezlov/dvfinanceexchanger/DvFinanceExchangerApplication.java
+++ src/main/java/dzhezlov/dvfinanceexchanger/DvFinanceExchangerApplication.java
@@ -1,13 +1,9 @@
 package dzhezlov.dvfinanceexchanger;
 
 import dzhezlov.dvfinanceexchanger.config.TradeProperties;
-import dzhezlov.dvfinanceexchanger.repository.TradeHistoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
@@ -18,18 +14,4 @@ public class DvFinanceExchangerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DvFinanceExchangerApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            try {
-                TradeHistoryRepository bean = ctx.getBean(TradeHistoryRepository.class);
-                bean.deleteAll();
-            } catch (Exception e) {
-
-            }
-        };
-    }
-
 }
